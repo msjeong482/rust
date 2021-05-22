5839d537330e9e8b9e9ad8431b45f0888af2f799
Minseong Jeong <msjeong482@gmail.com>
2021-05-23 08:27:48 +0900

    hello world

--- basic_projects/hello_world/main.rs
+++ basic_projects/hello_world/main.rs
@@ -1,3 +1,3 @@
-fn main() { // main function: starting point
-    println!("Hello, world!"); // with 4 spaces, ! symbol means macro, ; symbol means end of syntax.
+fn main() { 
+    println!("Hello, world!"); 
 }
