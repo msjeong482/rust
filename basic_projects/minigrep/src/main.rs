74fd7b31a58915dbd43aa88d0f5abdfcd26a5e3d
Minseong Jeong <msjeong482@gmail.com>
2021-06-08 07:27:33 +0900

    grouping config

--- basic_projects/minigrep/src/main.rs
+++ basic_projects/minigrep/src/main.rs
@@ -8,17 +8,22 @@ fn main() {
         panic!("the count of args is too small.. it must be greater than 3.");
     }
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("can not read a file.");
 
     println!("contents: {}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    (query, filename)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
