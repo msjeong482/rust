f8399c71e7ee056d2d14dacba90a27f1eb6b161d
Minseong Jeong <msjeong482@gmail.com>
2021-06-08 07:24:08 +0900

    separate parsing args

--- basic_projects/minigrep/src/main.rs
+++ basic_projects/minigrep/src/main.rs
@@ -8,11 +8,17 @@ fn main() {
         panic!("the count of args is too small.. it must be greater than 3.");
     }
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     let contents = fs::read_to_string(filename)
         .expect("can not read a file.");
 
     println!("contents: {}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
