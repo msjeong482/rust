57624e2aa2e6c7704d1e7bb0e0cf3caa7c817fad
Minseong Jeong <msjeong482@gmail.com>
2021-06-08 08:24:06 +0900

    separate library crate

---
+++ basic_projects/minigrep/src/lib.rs
@@ -0,0 +1,29 @@
+
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("the count of args is too small.. it must be greater than 3.");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("contents: \n{}", contents);
+
+    Ok(())
+}
--- basic_projects/minigrep/src/main.rs
+++ basic_projects/minigrep/src/main.rs
@@ -1,8 +1,8 @@
 
-use std::error::Error;
-use std::process;
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,34 +12,9 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("application error: {}", e);
         process::exit(1);
     }
 }
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("the count of args is too small.. it must be greater than 3.");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("contents: \n{}", contents);
-
-    Ok(())
-}
