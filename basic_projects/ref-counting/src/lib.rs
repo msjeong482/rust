18fe25a33e614bf6fd6818c927c2b2adf89f06cf
Minseong Jeong <msjeong482@gmail.com>
2021-06-15 08:44:15 +0900

    reference cell

--- basic_projects/ref-counting/src/lib.rs
+++ basic_projects/ref-counting/src/lib.rs
@@ -32,3 +32,33 @@ impl<'a, T> LimitTracker<'a, T>
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger { sent_messages: RefCell::new(vec![]) }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, msg: &str) {
+            self.sent_messages.borrow_mut().push(String::from(msg));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+        limit_tracker.set_value(80);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
+}
--- basic_projects/ref-counting/src/main.rs
+++ basic_projects/ref-counting/src/main.rs
@@ -1,25 +1,24 @@
-
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
+use std::cell::RefCell;
 use List::{Cons, Nil};
 use std::rc::Rc;
 
 fn main() {
-    let a = Rc::new(Cons(5,
-        Rc::new(Cons(10,
-            Rc::new(Nil)))));
+    let value = Rc::new(RefCell::new(5));
 
-    println!("the counter after createing a = {}", Rc::strong_count(&a));
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
 
-    let b = Cons(3, Rc::clone(&a));
-    println!("the counter after createing b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("the counter after createing c = {}", Rc::strong_count(&a));
-    }
+    *value.borrow_mut() += 10;
 
-    println!("the counter after drop c = {}", Rc::strong_count(&a));
+    println!("after a = {:?}", a);
+    println!("after b = {:?}", b);
+    println!("after c = {:?}", c);
+
 }
